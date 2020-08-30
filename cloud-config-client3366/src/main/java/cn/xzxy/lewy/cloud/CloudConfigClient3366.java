174c8301693b3a7f0f8dd368a65878845588e87f
lewy95 <171133755@qq.com>
2020-08-30 17:03:38 +0800

    fix/to: add config + bus

---
+++ cloud-config-client3366/src/main/java/cn/xzxy/lewy/cloud/CloudConfigClient3366.java
@@ -0,0 +1,13 @@
+package cn.xzxy.lewy.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudConfigClient3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigClient3366.class, args);
+    }
+}
