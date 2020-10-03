a0c07a4bb712ad823d8701df948947f302a618d7
lewy95 <171133755@qq.com>
2020-10-04 00:33:33 +0800

    fix/to: add nacos module

---
+++ cloud-alibaba-nacos-config-client3377/src/main/java/cn/xzxy/lewy/cloud/CloudNacosConfigClient3377.java
@@ -0,0 +1,13 @@
+package cn.xzxy.lewy.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudNacosConfigClient3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNacosConfigClient3377.class, args);
+    }
+}
